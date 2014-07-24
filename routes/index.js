653469e6f70720dcd8f7809b5a81aa687295c926
Sam Backus <Samuel.Backus@gmail.com>
2014-07-24 16:32:15 -0400

    save scenes in story array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+var story = []
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res){
+  story.push(req.body.scene);
+  res.send(story);
 });
 
 module.exports = router;
